bae2ad021635778957f351cce6d810a7e9d1fa0b
su-giana <giananews@gmail.com>
2024-01-26 20:14:53 +0900

    feat : create CRUD service of Customer Object

--- src/main/java/com/swifty/bank/server/core/customer/service/CustomerService.java
+++ src/main/java/com/swifty/bank/server/core/customer/service/CustomerService.java
@@ -1,8 +1,14 @@
-package com.swifty.bank.server.src.main.core.customer.service;
+package com.swifty.bank.server.core.customer.service;
 
-import com.swifty.bank.server.src.main.core.customer.dto.CustomerJoinDto;
+import com.swifty.bank.server.core.customer.Customer;
+import com.swifty.bank.server.core.customer.dto.CustomerFindDto;
+import com.swifty.bank.server.core.customer.dto.CustomerJoinDto;
+
 import java.util.UUID;
 
 public interface CustomerService {
     void join(CustomerJoinDto customerJoinDto);
+    Customer find(CustomerFindDto uuid);
+    Customer updatePhoneNumber(CustomerJoinDto customerJoinDto);
+    void withdrawCustomer(CustomerFindDto uuid);
 }
