9dde4c14a8db4ed4ae7187377accba7cabe56afb
su-giana <giananews@gmail.com>
2024-01-26 20:13:29 +0900

    feat : add UUID to DTO

--- src/main/java/com/swifty/bank/server/core/customer/dto/CustomerJoinDto.java
+++ src/main/java/com/swifty/bank/server/core/customer/dto/CustomerJoinDto.java
@@ -1,10 +1,13 @@
-package com.swifty.bank.server.src.main.core.customer.dto;
+package com.swifty.bank.server.core.customer.dto;
 
-import com.swifty.bank.server.src.main.core.customer.constant.Nationality;
+import com.swifty.bank.server.core.customer.constant.Nationality;
 import lombok.Getter;
+
+import java.util.UUID;
 
 @Getter
 public class CustomerJoinDto {
+    private UUID uuid;
     private String name;
     private Nationality nationality;
     private String phoneNumber;
