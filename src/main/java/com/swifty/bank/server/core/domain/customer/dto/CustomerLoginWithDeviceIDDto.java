099fc26eda1f6a15c2d7f534a9c4198c44f7e77f
su-giana <giananews@gmail.com>
2024-02-01 17:15:50 +0900

    feat : add NoArgsConstruct to dto

--- src/main/java/com/swifty/bank/server/core/domain/customer/dto/CustomerLoginWithDeviceIDDto.java
+++ src/main/java/com/swifty/bank/server/core/domain/customer/dto/CustomerLoginWithDeviceIDDto.java
@@ -2,9 +2,11 @@ package com.swifty.bank.server.core.domain.customer.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomerLoginWithDeviceIDDto {
     private String deviceID;
 }
