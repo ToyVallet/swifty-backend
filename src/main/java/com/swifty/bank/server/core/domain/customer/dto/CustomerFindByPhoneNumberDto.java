81932c8226bb0955e41c28da09992ce35d247d56
su-giana <giananews@gmail.com>
2024-02-01 17:15:10 +0900

    feat : change from phoneNumberAndNationlity to phoneNumber

--- src/main/java/com/swifty/bank/server/core/domain/customer/dto/CustomerFindByPhoneNumberDto.java
+++ src/main/java/com/swifty/bank/server/core/domain/customer/dto/CustomerFindByPhoneNumberDto.java
@@ -1,11 +1,10 @@
 package com.swifty.bank.server.core.domain.customer.dto;
 
-import com.swifty.bank.server.core.domain.customer.constant.Nationality;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class CustomerFindByPhoneNumber {
+public class CustomerFindByPhoneNumberDto {
     private String phoneNumber;
 }
