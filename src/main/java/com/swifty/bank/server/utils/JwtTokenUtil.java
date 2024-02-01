00f9c54edf983e6b6d63f0891377b502df3377ee
su-giana <giananews@gmail.com>
2024-02-01 17:13:03 +0900

    feat : add method returning Authentication object from token

--- src/main/java/com/swifty/bank/server/utils/JwtTokenUtil.java
+++ src/main/java/com/swifty/bank/server/utils/JwtTokenUtil.java
@@ -5,19 +5,26 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("serial")
 @Component
 public class JwtTokenUtil implements Serializable {
     @Value("${jwt.secret}")
-    private static String secretKey;
+    private String secretKey;
     private int expiration = 15 * 60;
 
     public String getUuidFromToken(String token) {
@@ -64,8 +71,8 @@ public class JwtTokenUtil implements Serializable {
         claims.put("scopes", Arrays.asList(new SimpleGrantedAuthority("CUSTOMER")));
 
         return Jwts.builder()
-                .setSubject(subject)
                 .setClaims(claims)
+                .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
@@ -74,5 +81,24 @@ public class JwtTokenUtil implements Serializable {
     public Boolean validateToken(String token) {
         final String uuid = getUuidFromToken(token);
         return !uuid.isEmpty();
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = getClaimFromToken(accessToken);
+
+        if (claims.get("id").toString().isBlank() || claims.get("id").toString().isEmpty()) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get("scopes").toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication 리턴
+        String subject = claims.getSubject( );
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 }
