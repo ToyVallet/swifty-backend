3ca9cbf6b87fd752ee840764d42035adeed70bc8
su-giana <giananews@gmail.com>
2024-02-01 17:08:03 +0900

    feat : get rid of hashing in device id

--- src/main/java/com/swifty/bank/server/api/controller/CustomerController.java
+++ src/main/java/com/swifty/bank/server/api/controller/CustomerController.java
@@ -3,11 +3,10 @@ package com.swifty.bank.server.api.controller;
 import com.swifty.bank.server.core.domain.customer.Customer;
 import com.swifty.bank.server.core.domain.customer.dto.*;
 import com.swifty.bank.server.core.domain.customer.exceptions.NoSuchCustomerByDeviceID;
-import com.swifty.bank.server.core.domain.customer.exceptions.NoSuchCustomerByPhoneNumberAndNationality;
+import com.swifty.bank.server.core.domain.customer.exceptions.NoSuchCustomerByPhoneNumberException;
 import com.swifty.bank.server.core.domain.customer.service.CustomerService;
 import com.swifty.bank.server.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -20,13 +19,13 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping(value = "auth")
 public class CustomerController {
     private final CustomerService customerService;
-    @Autowired
     private final JwtTokenUtil tokenUtil;
     private final BCryptPasswordEncoder encoder;
 
-    @PostMapping(value = "sign_in")
+    @PostMapping(value = "sign-in")
     public ResponseEntity<?> login(
             @RequestHeader(value = "Authorization") String token,
             @RequestBody CustomerLoginWithDeviceIDDto body
@@ -45,10 +44,12 @@ public class CustomerController {
         }
 
         UUID uuid = UUID.fromString(tokenUtil.getUuidFromToken(token));
-        Customer customer = customerService.findByDeviceID(new CustomerFindByDeviceIDDto(body.getDeviceID( )));
+        Customer customer = customerService.findByDeviceID(new CustomerFindByDeviceIDDto(
+                body.getDeviceID( ))
+        );
 
         if (uuid.toString( ).equals(customer.getId( ).toString( ))
-         && customer.getDeviceID().equals(encoder.encode(body.getDeviceID( )))) {
+         && customer.getDeviceID().equals(body.getDeviceID( ))) {
             result.put("token", tokenUtil.generateToken(customer));
             return ResponseEntity
                     .ok()
@@ -60,68 +61,67 @@ public class CustomerController {
                 .body("[ERROR] Token's UUID and device's original UUID is different");
     }
 
-    @PostMapping(value = "auth")
+    @PostMapping("")
     public ResponseEntity<?> authWithForm(
             @RequestBody CustomerJoinDto body
     ) {
         Map<String, Object> res = new HashMap<>( );
 
-        String deviceID = encoder.encode(body.getDeviceID( ));
 
         Customer customerByDeviceID;
-        Customer customerByPhoneNumberAndNationality;
+        Customer customerByPhoneNumber;
 
         try {
             customerByDeviceID = customerService
-                    .findByDeviceID(new CustomerFindByDeviceIDDto(deviceID));
+                    .findByDeviceID(new CustomerFindByDeviceIDDto(body.getDeviceID()));
         }
         catch (NoSuchCustomerByDeviceID e) {
             customerByDeviceID = null;
         }
 
         try {
-            customerByPhoneNumberAndNationality = customerService
-                    .findByPhoneNumberAndNationality(
-                            new CustomerFindByPhoneNumberAndNationality(body.getPhoneNumber( ), body.getNationality( ))
+            customerByPhoneNumber = customerService
+                    .findByPhoneNumber(
+                            new CustomerFindByPhoneNumberDto(body.getPhoneNumber( ))
                     );
         }
-        catch (NoSuchCustomerByPhoneNumberAndNationality e) {
-            customerByPhoneNumberAndNationality = null;
+        catch (NoSuchCustomerByPhoneNumberException e) {
+            customerByPhoneNumber = null;
         }
 
         Customer customer = null;
-        if (customerByPhoneNumberAndNationality == null && customerByDeviceID == null) {
+        if (customerByPhoneNumber == null && customerByDeviceID == null) {
             CustomerJoinDto dto = new CustomerJoinDto(
                     body.getUuid(),
                     body.getName(),
                     body.getNationality(),
                     body.getPhoneNumber(),
-                    body.getPassword(),
-                    deviceID
+                    encoder.encode(body.getPassword()),
+                    body.getDeviceID()
             );
             customer = customerService.join(dto);
         }
-        else if (customerByPhoneNumberAndNationality == null) {
-            CustomerUpdatePhoneNumberAndNationalityDto dto = new CustomerUpdatePhoneNumberAndNationalityDto(
-                    body.getUuid(),  body.getNationality( ), body.getPhoneNumber( )
+        else if (customerByPhoneNumber == null) {
+            CustomerUpdatePhoneNumberDto dto = new CustomerUpdatePhoneNumberDto(
+                    body.getUuid(), body.getPhoneNumber( )
             );
-            customer = customerService.updatePhoneNumberAndNationality(dto);
+            customer = customerService.updatePhoneNumber(dto);
         }
         else if (customerByDeviceID == null) {
             CustomerUpdateDeviceIDDto dto = new CustomerUpdateDeviceIDDto(
-                    body.getUuid( ), deviceID
+                    body.getUuid( ), body.getDeviceID( )
             );
             customer = customerService.updateDeviceID(dto);
         }
         else {
-            if (customerByDeviceID.getId( ).equals(customerByPhoneNumberAndNationality.getId( ))) {
+            if (customerByDeviceID.getId( ).equals(customerByPhoneNumber.getId( ))) {
                 customer = customerByDeviceID;
             }
             else {
 
                 customer = customerService.updateDeviceID(new CustomerUpdateDeviceIDDto(
-                        customerByPhoneNumberAndNationality.getId( ),
-                        encoder.encode(customerByPhoneNumberAndNationality.getDeviceID( ))
+                        customerByPhoneNumber.getId( ),
+                        customerByPhoneNumber.getDeviceID( )
                 ));
                 customerService.updateDeviceID(new CustomerUpdateDeviceIDDto(
                         customerByDeviceID.getId( ),
@@ -135,16 +135,4 @@ public class CustomerController {
                 .ok( )
                 .body(res);
     }
-
-
-//    @GetMapping(value = "find")
-//    public ResponseEntity find(@RequestBody CustomerFindDto uuid) {
-//        try {
-//            return new ResponseEntity(customerService.find(uuid), HttpStatus.OK);
-//        }
-//        catch (Exception e) {
-//            return new ResponseEntity(e.getMessage(), HttpStatus.OK);
-//        }
-//    }
-//
 }
