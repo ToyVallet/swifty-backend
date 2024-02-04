cf7e4b9a9359402127b16fecc67c93d54d06d578
su-giana <giananews@gmail.com>
2024-02-04 09:58:48 +0900

    refactor: change parameter to specific parameter such as string not dto if necessary for re-use

--- src/main/java/com/swifty/bank/server/core/domain/customer/service/CustomerService.java
+++ src/main/java/com/swifty/bank/server/core/domain/customer/service/CustomerService.java
@@ -1,25 +1,26 @@
 package com.swifty.bank.server.core.domain.customer.service;
 
+import com.swifty.bank.server.core.domain.customer.dto.JoinRequest;
 import com.swifty.bank.server.core.domain.customer.Customer;
-import com.swifty.bank.server.core.domain.customer.dto.*;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 public interface CustomerService {
-    Customer join(CustomerJoinDto customerJoinDto);
+    Customer join(JoinRequest joinRequest);
 
     // 순수 UUID로의 조회를 1원칙으로 삼는다
     // Principle 1. Retrieve with User's own UUID (PK)
     // Something to exchange with Frontend as user identification
     // Send access token(JWT) to frontend with encrypted UUID
     // Condition of Retrieval : JPQL
-    Customer findById(CustomerFindDto uuid);
+    Customer findByUuid(UUID uuid);
 
-    Customer findByPhoneNumber(CustomerFindByPhoneNumberDto dto);
+    Customer findByPhoneNumber(String phoneNumber);
 
-    Customer findByDeviceID(CustomerFindByDeviceIDDto dto);
+    Customer findByDeviceID(String deviceId);
 
-    Customer updatePhoneNumber(CustomerUpdatePhoneNumberDto dto);
+    Customer updatePhoneNumber(UUID uuid, String phoneNumber);
 
-    Customer updateDeviceID(CustomerUpdateDeviceIDDto dto);
-    void withdrawCustomer(CustomerFindDto uuid);
+    Customer updateDeviceID(UUID uuid, String deviceId);
+    void withdrawCustomer(UUID uuid);
 }
