bcbf370711f112adab5d4c67a46e12ac9a4d2e1a
su-giana <giananews@gmail.com>
2024-02-04 12:56:14 +0900

    feat: create logic handling request of specific annotation's method

--- src/main/java/com/swifty/bank/server/core/common/authentication/JwtAuthenticationInterceptor.java
+++ src/main/java/com/swifty/bank/server/core/common/authentication/JwtAuthenticationInterceptor.java
@@ -1,8 +1,10 @@
 package com.swifty.bank.server.core.common.authentication;
 
+import com.swifty.bank.server.core.common.authentication.annotation.PassAuth;
 import com.swifty.bank.server.core.common.authentication.exception.TokenContentNotValidException;
 import com.swifty.bank.server.core.common.authentication.exception.TokenFormatNotValidException;
 import com.swifty.bank.server.core.domain.customer.Customer;
+import com.swifty.bank.server.core.domain.customer.exceptions.NoSuchCustomerByUUID;
 import com.swifty.bank.server.core.domain.customer.repository.CustomerRepository;
 import com.swifty.bank.server.core.domain.customer.service.CustomerService;
 import com.swifty.bank.server.utils.JwtTokenUtil;
@@ -10,7 +12,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import java.util.UUID;
 
@@ -22,16 +26,19 @@ public class JwtAuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
+        if (checkAnnotation(handler, PassAuth.class)) {
+            res.setStatus(200);
+            return true;
+        }
+
         try {
+            String uri = req.getRequestURI();
             UUID uuid = jwtTokenUtil.getUuidFromToken(
-                    req.getHeader("Authentication")
+                    req.getHeader("Authorization")
             );
 
             Customer customer = customerService.findByUuid(uuid);
-            res.sendError(
-                    HttpServletResponse.SC_OK,
-                    "[ERROR] Customer doesn't exist"
-            );
+            res.setStatus(200);
             return true;
         }
         catch (TokenFormatNotValidException e) {
@@ -46,6 +53,27 @@ public class JwtAuthenticationInterceptor implements HandlerInterceptor {
                     e.getMessage( )
             );
         }
+        catch (NoSuchCustomerByUUID e) {
+            res.sendError(
+                    HttpServletResponse.SC_OK,
+                    e.getMessage( )
+            );
+        }
+        return false;
+    }
+
+    private boolean checkAnnotation(Object handler, Class<PassAuth> passAuthClass) {
+        if (handler instanceof ResourceHttpRequestHandler) {
+            return true;
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+        if (handlerMethod.getMethodAnnotation(passAuthClass) != null
+         || null != handlerMethod.getBeanType().getAnnotation(passAuthClass)) {
+            return true;
+        }
+
         return false;
     }
 }
