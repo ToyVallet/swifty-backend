c9280d9104d1b47e8ea35eef6a809ed846187777
threepebbles <threepebbles@naver.com>
2024-02-05 22:36:37 +0900

    refactor: use @Getter annotation

--- src/main/java/com/swifty/bank/server/core/domain/sms/constant/MessageStatus.java
+++ src/main/java/com/swifty/bank/server/core/domain/sms/constant/MessageStatus.java
@@ -2,7 +2,9 @@ package com.swifty.bank.server.core.domain.sms.constant;
 
 import com.twilio.rest.api.v2010.account.Message.Status;
 import java.util.Arrays;
+import lombok.Getter;
 
+@Getter
 public enum MessageStatus {
     QUEUED("queued"),
     SENDING("sending"),
@@ -11,12 +13,8 @@ public enum MessageStatus {
 
     private final String status;
 
-    private MessageStatus(final String status) {
+    MessageStatus(final String status) {
         this.status = status;
-    }
-
-    public String getStatus() {
-        return status;
     }
 
     public static MessageStatus findByTwilioMessageStatus(Status twilioMessageStatus) {
