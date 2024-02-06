3422d02adf047d9f9a1632e141c66465e568a87a
HSBODo <vinsulill@gmaile.com>
2024-02-06 12:50:36 +0900

    feat: customer 엔티티 변수 추가 및 메소드 추가

--- src/main/java/com/swifty/bank/server/core/domain/customer/Customer.java
+++ src/main/java/com/swifty/bank/server/core/domain/customer/Customer.java
@@ -2,58 +2,63 @@ package com.swifty.bank.server.core.domain.customer;
 
 import com.swifty.bank.server.core.domain.BaseEntity;
 import com.swifty.bank.server.core.domain.customer.constant.CustomerStatus;
+import com.swifty.bank.server.core.domain.customer.constant.Gender;
 import com.swifty.bank.server.core.domain.customer.constant.Nationality;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.Table;
-import java.util.UUID;
+import jakarta.persistence.*;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-@Data
+import java.util.UUID;
+
+@Getter
 @Entity
 @Table(name = "tb_customer")
 @NoArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
 @EntityListeners(AuditingEntityListener.class)
 public class Customer extends BaseEntity {
+
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(columnDefinition = "BINARY(16)")
     @Id
     private UUID id;    // PK
-    private String name;
-    private String phoneNumber;
-    @Enumerated(EnumType.STRING)
-    private Nationality nationality;
-    @Enumerated(EnumType.STRING)
-    private CustomerStatus customerStatus;  // 휴면 상태, 정지된 사용자 등
-    private String password;
-    private GrantedAuthority roles;
+
     private String deviceId;
 
+    private String name;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Nationality nationality;
+
+    @Enumerated(EnumType.STRING)
+    private CustomerStatus customerStatus;  // 휴면 상태, 정지된 사용자 등
+
+    private String password;
+
+    private GrantedAuthority roles;
+
     @Builder
-    public Customer(UUID id, String name, String phoneNumber, Nationality nationality,
-                    CustomerStatus customerStatus, String password, String deviceId) {
+    public Customer(UUID id, String deviceId, String name, String phoneNumber, Gender gender, String birthDate, Nationality nationality, CustomerStatus customerStatus, String password, GrantedAuthority roles) {
         this.id = id;
+        this.deviceId = deviceId;
         this.name = name;
         this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.birthDate = birthDate;
         this.nationality = nationality;
         this.customerStatus = customerStatus;
         this.password = password;
-        this.roles = new SimpleGrantedAuthority("CUSTOMER");
-        this.deviceId = deviceId;
+        this.roles = roles;
     }
 
     public void updatePhoneNumber(String phoneNumber) {
@@ -66,6 +71,18 @@ public class Customer extends BaseEntity {
 
     public void updateDeviceId(String deviceId) {
         this.deviceId = deviceId;
+    }
+
+    public void updateName(String newName) {
+        this.name = newName;
+    }
+
+    public void updateBirthDate(String newBirthDate) {
+        this.birthDate = newBirthDate;
+    }
+
+    public void resetPassword(String newPassword) {
+        this.password = newPassword;
     }
 
 }
