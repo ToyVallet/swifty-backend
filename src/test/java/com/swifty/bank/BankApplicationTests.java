4933fa10557b964715aaaa7c4ccb8531a52c0e1d
su-giana <giananews@gmail.com>
2024-02-09 23:24:03 +0900

    feat: add validation in dto

--- src/test/java/com/swifty/bank/BankApplicationTests.java
+++ src/test/java/com/swifty/bank/BankApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
 
-	}
+    }
 
 }
