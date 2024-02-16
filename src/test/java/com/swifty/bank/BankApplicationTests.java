6f2e5c89a7f443f009a9041b6e13a4684e755318
threepebbles <threepebbles@naver.com>
2024-02-16 22:22:53 +0900

    test: nothing but test ing workflow

--- src/test/java/com/swifty/bank/BankApplicationTests.java
+++ src/test/java/com/swifty/bank/BankApplicationTests.java
@@ -1,14 +1,13 @@
 package com.swifty.bank;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankApplicationTests {
 
-    @Test
-    void contextLoads() {
-
-    }
+//    @Test
+//    void contextLoads() {
+//
+//    }
 
 }
