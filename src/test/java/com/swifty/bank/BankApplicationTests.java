eaff86174fc89a7d7bde7991edd45718d4000dd8
threepebbles <threepebbles@naver.com>
2024-02-19 02:57:57 +0900

    test: fix test code error

--- src/test/java/com/swifty/bank/BankApplicationTests.java
+++ src/test/java/com/swifty/bank/BankApplicationTests.java
@@ -3,7 +3,7 @@ package com.swifty.bank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BankApplicationTests.class)
 class BankApplicationTests {
 
     @Test
