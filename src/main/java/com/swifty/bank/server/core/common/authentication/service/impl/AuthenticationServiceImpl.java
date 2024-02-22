ff224350da95c521b9cc705810f5e0aa03859487
HSBODo <vinsulill@gmaile.com>
2024-02-22 19:12:52 +0900

    refactor: jwtutil을 jwtservice로 교체

--- src/main/java/com/swifty/bank/server/core/common/authentication/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/swifty/bank/server/core/common/authentication/service/impl/AuthenticationServiceImpl.java
@@ -2,77 +2,93 @@ package com.swifty.bank.server.core.common.authentication.service.impl;
 
 import com.swifty.bank.server.core.common.authentication.Auth;
 import com.swifty.bank.server.core.common.authentication.dto.TokenDto;
-import com.swifty.bank.server.core.common.authentication.exception.TokenContentNotValidException;
+import com.swifty.bank.server.core.common.authentication.exception.NoSuchAuthByUuidException;
+import com.swifty.bank.server.core.common.authentication.exception.NotLoggedInCustomerException;
+import com.swifty.bank.server.core.common.authentication.repository.AuthRepository;
 import com.swifty.bank.server.core.common.authentication.service.AuthenticationService;
+import com.swifty.bank.server.core.common.service.JwtService;
 import com.swifty.bank.server.core.domain.customer.Customer;
-import com.swifty.bank.server.utils.DateUtil;
-import com.swifty.bank.server.utils.JwtUtil;
 import com.swifty.bank.server.utils.RedisUtil;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final RedisUtil redisUtil;
-    private final JwtUtil jwtUtil;
+    private final JwtService jwtService;
+    private final AuthRepository authRepository;
 
-    @Value("${jwt.access-token-expiration-millis}")
-    private int accessTokenExpiration;
-    @Value("${jwt.refresh-token-expiration-millis}")
-    private int refreshTokenExpiration;
 
     @Override
     public TokenDto generateTokenDtoWithCustomer(Customer customer) {
         TokenDto tokens = new TokenDto(createAccessToken(customer), createRefreshToken(customer));
-        saveRefreshTokenInRedis(tokens.getRefreshToken());
         return tokens;
     }
 
-    private String createAccessToken(Customer customer) {
-        Claims claims = Jwts.claims();
-        Date now = DateUtil.now();
+    @Override
+    public void logout(UUID uuid) {
+        if (!isLoggedOut(uuid)) {
+            String key = uuid.toString();
+            Auth prevAuth = redisUtil.getRedisAuthValue(key);
+            if (prevAuth == null) {
+                prevAuth = authRepository.findAuthByUuid(uuid)
+                        .orElseThrow(() -> new NoSuchAuthByUuidException("[ERROR] 해당 유저의 로그인 정보가 없습니다."));
+            }
 
-        claims.put("id", customer.getId());
-        claims.put("scopes", List.of(new SimpleGrantedAuthority("CUSTOMER")));
-        claims.setSubject("ACCESS");
-        claims.setExpiration(DateUtil.millisToDate(now.getTime() + accessTokenExpiration * 1000L));
-        return jwtUtil.generateToken(claims);
+            prevAuth.updateAuthContent("LOGOUT");
+            redisUtil.saveAuthRedis(key, prevAuth);
+        }
+        throw new NotLoggedInCustomerException("[ERROR] 로그인 되지 않은 유저가 로그 아웃을 시도했습니다.");
+    }
+
+    @Override
+    public boolean isLoggedOut(UUID uuid) {
+        Auth res = redisUtil.getRedisAuthValue(uuid.toString());
+        if (res == null) {
+            res = findAuthByUuid(uuid)
+                    .orElseThrow(() -> new NoSuchAuthByUuidException("[ERROR] 해당 유저의 로그인 정보가 없습니다."));
+            redisUtil.saveAuthRedis(uuid.toString(), res);
+        }
+        return res.getRefreshToken().equals("LOGOUT");
+    }
+
+    @Override
+    public Optional<Auth> findAuthByUuid(UUID uuid) {
+        return authRepository.findAuthByUuid(uuid);
+    }
+
+    private String createAccessToken(Customer customer) {
+        return jwtService.createJwtAccessToken(customer.getId());
     }
 
     private String createRefreshToken(Customer customer) {
-        Claims claims = Jwts.claims();
-        Date now = DateUtil.now();
-
-        claims.put("id", customer.getId());
-        claims.put("scopes", List.of(new SimpleGrantedAuthority("CUSTOMER")));
-        claims.setSubject("REFRESH");
-        claims.setExpiration(DateUtil.millisToDate(now.getTime() + refreshTokenExpiration * 1000L));
-        return jwtUtil.generateToken(claims);
+        return jwtService.createJwtRefreshToken(customer.getId());
     }
 
-    private void saveRefreshTokenInRedis(String token) {
-        UUID uuid = UUID.fromString(jwtUtil.getClaimByKeyFromToken("id", token).toString());
+    @Override
+    @Transactional
+    public void saveRefreshTokenInDataSources(String token) {
+        UUID uuid = jwtService.getCustomerId();;
+
         Auth previousAuth = redisUtil.getRedisAuthValue(uuid.toString());
+        if (previousAuth == null) {
+            previousAuth = findAuthByUuid(uuid)
+                    .orElse(null);
+        }
         Auth newAuth;
 
         if (previousAuth != null) {
-            newAuth = new Auth(token, previousAuth.isLoggedOut());
-            UUID prevUuid = UUID.fromString(
-                    jwtUtil.getClaimByKeyFromToken("id", previousAuth.getRefreshToken()).toString());
-
-            if (!uuid.toString().equals(prevUuid.toString())) {
-                throw new TokenContentNotValidException("[ERROR] Two token's owner is different");
-            }
+            previousAuth.updateAuthContent(token);
+            newAuth = previousAuth;
         } else {
-            newAuth = new Auth(token, false);
+            newAuth = new Auth(uuid, token);
+            authRepository.save(newAuth);
         }
         redisUtil.saveAuthRedis(uuid.toString(), newAuth);
     }
