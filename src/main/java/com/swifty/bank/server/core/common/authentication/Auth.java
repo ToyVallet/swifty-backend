cfb50ed5754bc6c39761f811829b831b6372eba0
threepebbles <threepebbles@naver.com>
2024-02-22 20:15:20 +0900

    fix: add annotations required for use as entity

--- src/main/java/com/swifty/bank/server/core/common/authentication/Auth.java
+++ src/main/java/com/swifty/bank/server/core/common/authentication/Auth.java
@@ -2,16 +2,25 @@ package com.swifty.bank.server.core.common.authentication;
 
 import com.swifty.bank.server.core.domain.BaseEntity;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Data
+@Getter
+@Entity
+@Table(name = "tb_refresh_token")
 @AllArgsConstructor
 @NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@EntityListeners(AuditingEntityListener.class)
 public class Auth extends BaseEntity {
     @Id
     @Column(columnDefinition = "BINARY(16)")
