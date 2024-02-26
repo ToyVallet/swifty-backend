f99bd7b0bb0947cfa7ea48dde7c89f9b52f130b0
su-giana <giananews@gmail.com>
2024-02-26 18:17:53 +0900

    feat: add entity annotation to refreshTokenDb object

--- src/main/java/com/swifty/bank/server/core/common/authentication/RefreshTokenDb.java
+++ src/main/java/com/swifty/bank/server/core/common/authentication/RefreshTokenDb.java
@@ -1,6 +1,10 @@
 package com.swifty.bank.server.core.common.authentication;
 
 import com.swifty.bank.server.core.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Entity
+@Table(name = "tb_ref_token")
 public class RefreshTokenDb extends BaseEntity {
+    @Id
+    @Column(columnDefinition = "BINARY(16)")
     private UUID customerUuid;
+    @Column(columnDefinition = "VARCHAR(1024)")
     private String refreshToken;
 
     public void updateRefreshToken(String refreshToken) {
