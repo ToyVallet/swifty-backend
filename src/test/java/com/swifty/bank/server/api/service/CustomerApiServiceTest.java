c0b6fac9a198b1ff84ae0c33173e29199ebba4b8
threepebbles <threepebbles@naver.com>
2024-03-01 02:34:56 +0900

    test: 테스트 환경 세팅

--- src/test/java/com/swifty/bank/server/api/service/CustomerApiServiceTest.java
+++ src/test/java/com/swifty/bank/server/api/service/CustomerApiServiceTest.java
@@ -1,8 +1,11 @@
-package com.swifty.bank;
+package com.swifty.bank.server.api.service;
 
+import com.swifty.bank.server.core.domain.customer.repository.CustomerRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -12,7 +15,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
-public class CustomerApiServiceTest {
+class CustomerApiServiceTest {
     @Container
     private static MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.33")
             .withDatabaseName("bank_db")
@@ -33,9 +36,52 @@ public class CustomerApiServiceTest {
         registry.add("spring.data.redis.port", redisContainer::getExposedPorts);
     }
 
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CustomerApiService customerApiService;
+
+    @Autowired
+    BCryptPasswordEncoder encoder;
+
+//    @Test
+//    public void saveCustomer() {
+//        Customer customer = Customer.builder()
+//                .id(UUID.randomUUID())
+//                .name("테스터")
+//                .customerStatus(CustomerStatus.ACTIVE)  // 일단 default
+//                .nationality(Nationality.KOREA)
+//                .phoneNumber("01077131548")
+//                .password(encoder.encode("1234"))
+//                .deviceId("디바이스아이디")
+//                .build();
+//        customerRepository.save(customer);
+//    }
+
     @Test
     public void testEquals() {
         int a = 1;
         Assertions.assertThat(1).isEqualTo(a);
+    }
+
+    @Test
+    void getCustomerInfo() {
+    }
+
+    @Test
+    void customerInfoUpdate() {
+    }
+
+    @Test
+    void confirmPassword() {
+    }
+
+    @Test
+    void resetPassword() {
+    }
+
+    @Test
+    void customerWithdrawal() {
     }
 }
