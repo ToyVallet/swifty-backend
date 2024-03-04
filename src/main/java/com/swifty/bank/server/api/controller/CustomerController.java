dab1432554be6e5bb5f3b711cbef43524bb06b08
HSBODo <vinsulill@gmaile.com>
2024-03-04 23:40:31 +0900

    feat: CustomerController @Operation  @ApiResponses swagger 문서화

--- src/main/java/com/swifty/bank/server/api/controller/CustomerController.java
+++ src/main/java/com/swifty/bank/server/api/controller/CustomerController.java
@@ -5,12 +5,17 @@ import com.swifty.bank.server.api.controller.dto.MessageResponse;
 import com.swifty.bank.server.api.controller.dto.customer.request.CustomerInfoUpdateConditionRequest;
 import com.swifty.bank.server.api.controller.dto.customer.request.PasswordRequest;
 import com.swifty.bank.server.api.controller.dto.customer.response.CustomerInfoResponse;
+import com.swifty.bank.server.api.controller.dto.sms.response.StealVerificationCodeResponse;
 import com.swifty.bank.server.api.service.CustomerApiService;
 import com.swifty.bank.server.api.service.dto.ResponseResult;
 import com.swifty.bank.server.api.service.dto.Result;
 import com.swifty.bank.server.core.common.authentication.service.AuthenticationService;
 import com.swifty.bank.server.core.utils.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,15 +32,26 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Controller
 @RequestMapping(value = "/customer")
-@Tag(name = "Customer Information API")
+@Tag(name = "회원 API")
 @Slf4j
 public class CustomerController {
     private final CustomerApiService customerApiService;
-    private final JwtUtil jwtUtil;
 
     @CustomerAuth
     @GetMapping("")
-    @Operation(summary = "get customer's whole information in database", description = "no request body needed")
+    @Operation(summary = "회원정보 조회", description = "jwt access 토큰으로 회원정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원정보 정상 조회",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerInfoResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "회원이 존재하지 않음",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    })
+    })
     public ResponseEntity<?> customerInfo() {
         UUID customerId = JwtUtil.getValueByKeyWithObject(JwtUtil.extractJwtFromCurrentRequestHeader(), "customerId", UUID.class);
         CustomerInfoResponse customerInfo = customerApiService.getCustomerInfo(customerId);
@@ -47,7 +63,19 @@ public class CustomerController {
 
     @CustomerAuth
     @PatchMapping("")
-    @Operation(summary = "change customer's whole information in database", description = "specific DTO required")
+    @Operation(summary = "회원정보 수정", description = "jwt access 토큰과 CustomerInfoUpdateConditionRequest 회원정보를 수정합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원정보를 수정하였습니다.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "회원정보를 수정을 실패하였습니다..",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    })
+    })
     public ResponseEntity<?> customerInfoUpdate(
             @RequestBody CustomerInfoUpdateConditionRequest customerInfoUpdateCondition) {
         UUID customerId = JwtUtil.getValueByKeyWithObject(JwtUtil.extractJwtFromCurrentRequestHeader(), "customerId", UUID.class);
@@ -62,7 +90,19 @@ public class CustomerController {
 
     @CustomerAuth
     @PostMapping("/password")
-    @Operation(summary = "confirm whether input and original password matches", description = "password string needed")
+    @Operation(summary = "회원 비밀번호 일치여부 확인", description = "jwt access 토큰과 입력한 password로 비밀번호가 일치하는지 확인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "비밀번호가 일치합니다.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "비밀번호가 불일치합니다.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    })
+    })
     public ResponseEntity<?> passwordConfirm(@RequestBody PasswordRequest password) {
         UUID customerId = JwtUtil.getValueByKeyWithObject(JwtUtil.extractJwtFromCurrentRequestHeader(), "customerId", UUID.class);
 
@@ -81,7 +121,19 @@ public class CustomerController {
 
     @CustomerAuth
     @PatchMapping("/password")
-    @Operation(summary = "reset password with input", description = "password string needed in body")
+    @Operation(summary = "회원 비밀번호 변경", description = "jwt access 토큰과 입력한 신규 비밀번호로 변경")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "비밀번호를 변경하였습니다.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "비밀번호를 변경을 실패 하였습니다.",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class))
+                    })
+    })
     public ResponseEntity<?> passwordReset(@RequestBody PasswordRequest newPassword) {
         UUID customerId = JwtUtil.getValueByKeyWithObject(JwtUtil.extractJwtFromCurrentRequestHeader(), "customerId", UUID.class);
 
