9ca36cbf1bc556072454a334db2702c27d125143
threepebbles <threepebbles@naver.com>
2024-03-07 19:14:14 +0900

    feat(SecureKeypadOrderInverse): json deserializatdeion을 위해 NoArgsConstructor 어노테이션 추가

--- src/main/java/com/swifty/bank/server/core/common/redis/value/SecureKeypadOrderInverse.java
+++ src/main/java/com/swifty/bank/server/core/common/redis/value/SecureKeypadOrderInverse.java
@@ -4,9 +4,11 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class SecureKeypadOrderInverse {
     private List<Integer> keypadOrderInverse;
