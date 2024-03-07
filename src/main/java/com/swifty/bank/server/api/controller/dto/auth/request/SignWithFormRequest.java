e6061961a8e7718203572daa99c0a7d98c86af34
threepebbles <threepebbles@naver.com>
2024-03-07 19:19:37 +0900

    feat(SignWithForm): 보안 키패드 추가에 따른 회원가입/로그인 로직 변경
    - request 변경
      - String password -> List<Integer> pushedOrder
    - response 변경
      - boolean isAvailablePassword 추가
    - 보안 키패드 복호화 기능 추가
    - 기존 회원의 비밀번호와 device id 업데이트 하지 않던 버그 수정

--- src/main/java/com/swifty/bank/server/api/controller/dto/auth/request/SignWithFormRequest.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/auth/request/SignWithFormRequest.java
@@ -4,17 +4,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.Data;
 
 @Data
 @Schema(description = "Information for enroll or log in user")
 public class SignWithFormRequest {
     @NotNull
-    @NotBlank
     @Size(min = 6, max = 6)
-    private String password;
+    @Schema(description = "보안 키패드를 누른 순서", example = "[3, 3, 0, 1, 2, 9]")
+    private List<Integer> pushedOrder;
 
     @NotNull
     @NotBlank
+    @Schema(description = "device id")
     private String deviceId;
 }
