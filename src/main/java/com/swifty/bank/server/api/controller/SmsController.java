f7cfe79feee2c6f84a8902755dc8663e9d528c47
threepebbles <threepebbles@naver.com>
2024-03-07 15:56:03 +0900

    refactor(SmsController): RestController -> Controller 변경

--- src/main/java/com/swifty/bank/server/api/controller/SmsController.java
+++ src/main/java/com/swifty/bank/server/api/controller/SmsController.java
@@ -20,14 +20,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping(value = "/sms")
 @Tag(name = "SMS 인증 API")
