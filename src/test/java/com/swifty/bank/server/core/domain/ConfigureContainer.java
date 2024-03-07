1fc705f4e8e06276ccb4533b200d16fc8927ca50
su-giana <giananews@gmail.com>
2024-03-07 22:35:31 +0900

    chore: temporary commit for testing

--- src/test/java/com/swifty/bank/server/core/domain/ConfigureContainer.java
+++ src/test/java/com/swifty/bank/server/core/domain/ConfigureContainer.java
@@ -2,36 +2,43 @@ package com.swifty.bank.server.core.domain;
 
 import com.redis.testcontainers.RedisContainer;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootTest
 @Transactional
+@Testcontainers
 @Disabled
 @ContextConfiguration(initializers = ConfigureContainer.IntegrationTestInitializer.class)
+@ActiveProfiles("test")
 public class ConfigureContainer {
+
     @Container
-    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0.33")
+    private static final MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0.33")
             .withPassword("test")
             .withUsername("test")
             .withDatabaseName("bank_db");
 
     @Container
-    private static RedisContainer redisContainer = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6.2.6"));
+    private static final RedisContainer redisContainer = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6.2.6"));
+
 
     @BeforeAll
-    public static void setupContainer( ) {
+    public static void setupContainers( ) {
         mySQLContainer.start();
         redisContainer.start();
     }
@@ -46,11 +53,13 @@ public class ConfigureContainer {
 
             TestPropertyValues.of(properties).applyTo(applicationContext);
         }
+
         private void setDatabaseProperties(Map<String, String> properties) {
             properties.put("spring.datasource.url", mySQLContainer.getJdbcUrl());
             properties.put("spring.datasource.username", mySQLContainer.getUsername());
             properties.put("spring.datasource.password", mySQLContainer.getPassword());
         }
+
         private void setRedisProperties(Map<String, String> properties) {
             properties.put("spring.redis.host", redisContainer.getHost());
             properties.put("spring.redis.port", redisContainer.getFirstMappedPort().toString());
