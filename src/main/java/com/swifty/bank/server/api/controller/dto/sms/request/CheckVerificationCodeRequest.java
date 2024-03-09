30ab3d73b60dbbd98425d86338511fd68ac79dcd
su-giana <giananews@gmail.com>
2024-03-10 02:13:07 +0900

    feat: add constructor to build entity

--- src/main/java/com/swifty/bank/server/api/controller/dto/sms/request/CheckVerificationCodeRequest.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/sms/request/CheckVerificationCodeRequest.java
@@ -6,8 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CheckVerificationCodeRequest {
     @NotNull
