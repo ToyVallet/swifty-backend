ddcdcfc55c1f1d000998de7d365d6e40efe9c875
threepebbles <threepebbles@naver.com>
2024-03-10 22:28:11 +0900

    fix: CustomerApiServcieImpl 에러 수정

--- src/main/java/com/swifty/bank/server/api/controller/dto/customer/response/CustomerInfoResponse.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/customer/response/CustomerInfoResponse.java
@@ -9,9 +9,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CustomerInfoResponse {
     @NotNull
     @NotBlank
