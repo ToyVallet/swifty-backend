3c04b5193da404a371b694c05eeac0e6b729f49b
threepebbles <threepebbles@naver.com>
2024-03-12 00:33:02 +0900

    feat(SBoxUtil): 키값에 대한 검증 수정, 암/복호화 대상에 대한 검증 추가

--- src/main/java/com/swifty/bank/server/core/utils/SBoxUtil.java
+++ src/main/java/com/swifty/bank/server/core/utils/SBoxUtil.java
@@ -6,18 +6,31 @@ import java.util.Collections;
 import java.util.List;
 
 public class SBoxUtil {
-    public static void validateKey(List<Integer> key) {
-        Integer max = Collections.max(key);
-        Integer min = Collections.min(key);
 
-        if (!(min.equals(0) && max.equals(key.size() - 1))) {
-            throw new IllegalArgumentException("올바르지 않은 키값 입니다.");
+    // key는 [0,key.size()-1] 범위를 모두 포함한 경우만 허용
+    public static void validateKey(List<Integer> key) {
+        boolean isInvalid = false;
+        for (int i = 0; i < key.size(); i++) {
+            isInvalid |= !key.contains(i);
+        }
+
+        if (isInvalid) {
+            throw new IllegalArgumentException(key + ": 올바르지 않은 키값 입니다.");
+        }
+    }
+
+    public static void validateTarget(List<Integer> target, List<Integer> key) {
+        Integer min = Collections.min(key);
+        Integer max = Collections.max(key);
+
+        if (Collections.min(target) < min || Collections.max(target) > max) {
+            throw new IllegalArgumentException(target + ": 올바르지 않은 타겟입니다.");
         }
     }
 
     public static List<Integer> encrypt(List<Integer> plain, List<Integer> key) {
         validateKey(key);
-
+        validateTarget(plain, key);
         return new ArrayList<>() {{
             for (Integer p : plain) {
                 add(key.get(p));
@@ -27,6 +40,7 @@ public class SBoxUtil {
 
     public static List<Integer> decrypt(List<Integer> encrypted, List<Integer> key) {
         validateKey(key);
+        validateTarget(encrypted, key);
 
         List<Integer> inverse = Arrays.asList(new Integer[key.size()]);
         for (int i = 0; i < key.size(); i++) {
