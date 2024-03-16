68e58c45b7342f271e4f050c03b9d5361c6a8b6d
Swifty <threepebbles@naver.com>
2024-03-16 19:26:37 +0900

    feat: cookie appied to JwtInterceptor

--- src/main/java/com/swifty/bank/server/api/controller/dto/auth/request/CheckLoginAvailabilityRequest.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/auth/request/CheckLoginAvailabilityRequest.java
@@ -6,12 +6,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Schema(description = "회원가입/로그인 가능 여부 확인 폼")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CheckLoginAvailabilityRequest {
     @NotNull
     @NotBlank
--- src/main/java/com/swifty/bank/server/core/utils/CookieUtils.java
+++ src/main/java/com/swifty/bank/server/core/utils/CookieUtils.java
@@ -1,15 +1,21 @@
 package com.swifty.bank.server.core.utils;
 
 import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtils {
+    @Value("${domain}")
+    private static String domainAddr;
+
     public static ResponseCookie createCookie(String name, String token) {
         ResponseCookie cookie = ResponseCookie.from(name, token)
                 .httpOnly(false)
                 .maxAge(60 * 60 * 12)
+                .secure(false)
+                .sameSite("Lax")
                 .path("/")
                 .build();
 
