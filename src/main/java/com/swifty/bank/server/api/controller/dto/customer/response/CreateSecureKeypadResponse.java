8b6ca0b47ce955646cd4cef37d9a9a8695100d80
threepebbles <threepebbles@naver.com>
2024-03-19 05:34:02 +0900

    docs: swagger response 문서 수정

--- src/main/java/com/swifty/bank/server/api/controller/dto/customer/response/CreateSecureKeypadResponse.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/customer/response/CreateSecureKeypadResponse.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(description = "개인 식별 비밀번호 확인을 위한 키패드 이미지 제공")
 public class CreateSecureKeypadResponse {
-    @Schema(name = "개인 식별 비밀번호 확인을 위한 키패드 이미지 제공",
-            description = "순서가 섞인 키패드 이미지 리스트",
+    @Schema(description = "순서가 섞인 키패드 이미지 리스트",
             example = "['<svg ... > </svg>\r\n', ... , '<svg ... > </svg>\r\n']")
     private List<String> keypad;
-    
-    @Schema(name = "키패드를 특정하기 위한 JWT")
+
+    @Schema(description = "키패드를 특정하기 위한 JWT")
     private String keypadToken;
 }
