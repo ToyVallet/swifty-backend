f91178319d6fa66a26d71fc5315850efd9c5ce26
threepebbles <threepebbles@naver.com>
2024-03-19 02:17:13 +0900

    feat: UnitedAccount 비밀번호 수정 요청 폼 수정

--- src/main/java/com/swifty/bank/server/api/controller/dto/account/request/ReviseUnitedAccountPasswordRequest.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/account/request/ReviseUnitedAccountPasswordRequest.java
@@ -1,13 +1,23 @@
 package com.swifty.bank.server.api.controller.dto.account.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Getter
+@Schema(description = "UnitedAccount 비밀번호 수정 요청 폼")
 @NoArgsConstructor
 public class ReviseUnitedAccountPasswordRequest {
+    @NotNull
+    @Schema(description = "UnitedAccount의 UUID")
     private UUID accountUuid;
-    private String password;
+
+    @NotNull
+    @Size(min = 4, max = 4)
+    @Schema(description = "보안 키패드를 누른 순서", example = "[3, 7, 0, 4]")
+    private List<Integer> pushedOrder;
 }
