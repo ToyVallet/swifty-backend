a13f412da9acd722a5e4a36814a99dbb552393d9
threepebbles <threepebbles@naver.com>
2024-03-19 01:48:43 +0900

    feat: 개인 식별 비밀번호 입력을 위한 키패드 이미지 dto와 계좌 비밀번호 입력을 위한 키패드 이미지 dto 분리

--- src/main/java/com/swifty/bank/server/api/controller/dto/account/response/CreateSecureKeypadResponse.java
+++ src/main/java/com/swifty/bank/server/api/controller/dto/account/response/CreateSecureKeypadResponse.java
@@ -1,4 +1,4 @@
-package com.swifty.bank.server.api.controller.dto.auth.response;
+package com.swifty.bank.server.api.controller.dto.account.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class CreateSecureKeypadResponse {
-    @Schema(name = "개인 식별 비밀번호 입력을 위한 키패드 이미지 제공",
+    @Schema(name = "계좌 비밀번호 입력을 위한 키패드 이미지 제공",
             description = "순서가 섞인 키패드 이미지 리스트",
             example = "['<svg ... > </svg>\r\n', ... , '<svg ... > </svg>\r\n']")
     private List<String> keypad;
