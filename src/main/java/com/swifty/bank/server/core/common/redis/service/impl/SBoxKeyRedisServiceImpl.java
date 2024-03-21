f9ce3164eb53d010ee5e568e4034e6a42b101c2d
threepebbles <threepebbles@naver.com>
2024-03-21 17:11:41 +0900

    refactor: redis에 키패드 순서 저장하는 시간 수정

--- src/main/java/com/swifty/bank/server/core/common/redis/service/impl/SBoxKeyRedisServiceImpl.java
+++ src/main/java/com/swifty/bank/server/core/common/redis/service/impl/SBoxKeyRedisServiceImpl.java
@@ -5,6 +5,7 @@ import com.swifty.bank.server.core.common.redis.service.SBoxKeyRedisService;
 import com.swifty.bank.server.core.common.redis.value.SBoxKey;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,8 @@ public class SBoxKeyRedisServiceImpl implements SBoxKeyRedisService {
     public final String prefix = "[SK]";
     private final SBoxKeyRedisRepository SBoxKeyRedisRepository;
 
-    private final Long timeout = 3L;
+    @Value("${jwt.keypad-token-expiration-seconds}")
+    private Long timeout;
 
     @Override
     public SBoxKey getData(String key) {
@@ -22,12 +24,12 @@ public class SBoxKeyRedisServiceImpl implements SBoxKeyRedisService {
 
     @Override
     public void setData(String key, SBoxKey value) {
-        SBoxKeyRedisRepository.setData(prefix + key, value, timeout, TimeUnit.MINUTES);
+        SBoxKeyRedisRepository.setData(prefix + key, value, timeout, TimeUnit.SECONDS);
     }
 
     @Override
     public void setData(String key, SBoxKey value, Long timeout, TimeUnit timeUnit) {
-        SBoxKeyRedisRepository.setData(prefix + key, value, timeout, TimeUnit.MINUTES);
+        SBoxKeyRedisRepository.setData(prefix + key, value, timeout, timeUnit);
     }
 
     @Override
