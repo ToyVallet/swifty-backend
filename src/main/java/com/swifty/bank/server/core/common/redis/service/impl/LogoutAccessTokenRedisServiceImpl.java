2470b660933796ab762b80330248c5c51504612a
threepebbles <threepebbles@naver.com>
2024-03-21 17:04:37 +0900

    rename: 클래스명 수정

--- src/main/java/com/swifty/bank/server/core/common/redis/service/impl/LogoutAccessTokenRedisServiceImpl.java
+++ src/main/java/com/swifty/bank/server/core/common/redis/service/impl/LogoutAccessTokenRedisServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class LogoutAccessTokenRedisRedisServiceImpl implements LogoutAccessTokenRedisService {
+public class LogoutAccessTokenRedisServiceImpl implements LogoutAccessTokenRedisService {
     public final String prefix = "[LAT]";
     private final LogoutAccessTokenRedisRepository logoutAccessTokenRedisRepository;
 
